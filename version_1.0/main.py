b54d15dd9df884987f1042955c17fcca1bd878b7
paradoxnafi <paradoxnafi@gmail.com>
2018-08-30 19:31:53 +0600

    Some spelling mistakes were fixed.

--- version_1.0/main.py
+++ version_1.0/main.py
@@ -1,4 +1,4 @@
-# Rock Paper Ceaser game
+# Rock Paper Scissors game
 # Version 1.0
 
 import random
@@ -7,16 +7,16 @@ while True:
 	cpu_point = 0
 	player_point = 0
 
-	print("Welcome in RPC game by IUBAT-1810\nWhat is your name: ", end='')
+	print("Welcome in RPS game by IUBAT-1810\nWhat is your name: ", end='')
 	name = input()
 
 	print("Game point: ", end='')
 	point = int(input())
-	print("Choose between R, P, C.\nHere R represents Rock, P represents Paper, C represents Ceaser,")
+	print("Choose between R, P, S.\nHere R represents Rock, P represents Paper, S represents Scissors,")
 
 	while True:
 
-		items = ['R', 'P', 'C']
+		items = ['R', 'P', 'S']
 		cpu_choice = random.choice(items)
 
 		print("=> ", end='')
@@ -28,32 +28,32 @@ while True:
 		elif (cpu_choice is 'P' and player_choice is 'P') or (cpu_choice is 'P' and player_choice is 'p'):
 			cpu = 'Paper'
 			player = 'paper'
-		elif (cpu_choice is 'C' and player_choice is 'C') or (cpu_choice is 'C' and player_choice is 'c'):
-			cpu = 'Ceaser'
-			player = 'Ceaser'
+		elif (cpu_choice is 'S' and player_choice is 'S') or (cpu_choice is 'S' and player_choice is 's'):
+			cpu = 'Scissors'
+			player = 'Scissors'
 		elif (cpu_choice is 'R' and player_choice is 'P') or (cpu_choice is 'R' and player_choice is 'p'):
 			player_point += 1
 			cpu = 'Rock'
 			player = 'Paper'
-		elif (cpu_choice is 'R' and player_choice is 'C') or (cpu_choice is 'R' and player_choice is 'c'):
+		elif (cpu_choice is 'R' and player_choice is 'S') or (cpu_choice is 'R' and player_choice is 's'):
 			cpu_point += 1
 			cpu = 'Rock'
-			player = 'Ceaser'
+			player = 'Scissors'
 		elif (cpu_choice is 'P' and player_choice is 'R') or (cpu_choice is 'P' and player_choice is 'r'):
 			cpu_point += 1
 			cpu = 'Paper'
 			player = 'Rock'
-		elif (cpu_choice is 'P' and player_choice is 'C') or (cpu_choice is 'P' and player_choice is 'c'):
+		elif (cpu_choice is 'P' and player_choice is 'S') or (cpu_choice is 'P' and player_choice is 's'):
 			player_point += 1
 			cpu = 'Paper'
-			player = 'Ceaser'
-		elif (cpu_choice is 'C' and player_choice is 'R') or (cpu_choice is 'C' and player_choice is 'r'):
+			player = 'Scissors'
+		elif (cpu_choice is 'S' and player_choice is 'R') or (cpu_choice is 'S' and player_choice is 'r'):
 			player_point += 1
-			cpu = 'Ceaser'
+			cpu = 'Scissors'
 			player = 'Rock'
-		elif (cpu_choice is 'C' and player_choice is 'P') or (cpu_choice is 'C' and player_choice is 'p'):
+		elif (cpu_choice is 'S' and player_choice is 'P') or (cpu_choice is 'S' and player_choice is 'p'):
 			cpu_point += 1
-			cpu = 'Ceaser'
+			cpu = 'Scissors'
 			player = 'Paper'
 		else:
 			print("Invalid input")
